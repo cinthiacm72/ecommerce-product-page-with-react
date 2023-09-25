357b80cf01d6c64e0b7852f794b149f7908989d6
cinthiacm72 <cinthiacm@gmail.com>
2023-09-25 13:17:20 -0300

    intial code

---
+++ data.js
@@ -0,0 +1,71 @@
+export const DATA = [
+  {
+    id: 1,
+    brand: "Sneaker Company",
+    name: "Fall Limited Edition Sneakers",
+    description:
+      "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+    price: "$125.00",
+    thumbnails: [
+      {
+        img1: {
+          large: "../collections/image-product-1.jpg",
+          thumb: "../collections/image-product-1-thumbnail.jpg",
+        },
+        img2: {
+          large: "../collections/image-product-2.jpg",
+          thumb: "../collections/image-product-2-thumbnail.jpg",
+        },
+        img3: {
+          large: "../collections/image-product-3.jpg",
+          thumb: "../collections/image-product-3-thumbnail.jpg",
+        },
+        img4: {
+          large: "../collections/image-product-4.jpg",
+          thumb: "../collections/image-product-4-thumbnail.jpg",
+        },
+      },
+      ,
+    ],
+  },
+  {
+    id: 2,
+    brand: "Sneaker Company",
+    name: "Winter Limited Edition Sneakers",
+    description:
+      "These high-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+    price: "$500.00",
+    thumbnails: [
+      {
+        img1: {
+          large: "../collections/image-product-1-blue.jpg",
+          thumb: "../collections/image-product-1-blue-thumbnail.jpg",
+        },
+        img2: {
+          large: "../collections/image-product-2-blue.jpg",
+          thumb: "../collections/image-product-2-blue-thumbnail.jpg",
+        },
+        img3: {
+          large: "../collections/image-product-3.jpg",
+          thumb: "../collections/image-product-3-thumbnail.jpg",
+        },
+      },
+      ,
+    ],
+  },
+  {
+    id: 3,
+    brand: "Sneaker Company",
+    name: "Summer Edition Sneakers",
+    description:
+      "These high-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+    price: "$500.00",
+    thumbnails: [
+      {
+        img1: {
+          large: "../collections/image-product-1-blue.jpg",
+        },
+      },
+    ],
+  },
+];
